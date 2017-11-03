f55ee773c2a22da0be6f5c5df53d0142d86c39ad
Anna <anna@changfamily.org>
2017-11-03 15:47:11 -0700

    Chartsv1
    Charts - Currently contains pie charts

--- src/ErrorAnalysis.js
+++ src/ErrorAnalysis.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import "./error-analysis.css";
+import { render } from 'react-dom';
+import { Chart } from 'react-google-charts';
 
 class ErrorAnalysis extends Component{
+    
     render() {
         return (
             <div className="container">
@@ -9,8 +12,17 @@ class ErrorAnalysis extends Component{
                     <h1>Error Analysis </h1>
                 </div>
                 <div className="graph-c">
-                    Graph
+                    <Chart
+                        chartType="PieChart"
+                        data={[['Error Type', 'Percentage'], ['Error a', 40], ['Error b', 10], ['Error c', 20], ['Error d', 8], ['Error e', 12], ['Error f', 10]]}
+                        options ={{}}
+                        graph_id="PieChart"
+                        width="100%"
+                        height="500px"
+                        legend_toggle
+                    />
                 </div>
+                    
             </div>    
         );
     }
